3348a7ff876111f930d3995b9b2792c9a91a8220
wgbbiao <wgb237@163.com>
2024-02-16 15:12:41 +0800

    添加

--- client.go
+++ client.go
@@ -221,6 +221,40 @@ func (c *Client) ReadHoldingRegisters(slaveID byte, address, quantity uint16) (r
 }
 
 // Request:
+//  Function code         : 1 byte (0x04)
+//  Starting address      : 2 bytes
+//  Quantity of registers : 2 bytes
+// Response:
+//  Function code         : 1 byte (0x04)
+//  Byte count            : 1 byte
+//  Input registers       : N bytes
+func (c *client) ReadInputRegisters(address, quantity uint16) (results []byte, err error) {
+	if quantity < 1 || quantity > 125 {
+		err = fmt.Errorf("modbus: quantity '%v' must be between '%v' and '%v',", quantity, 1, 125)
+		return
+	}
+	data := []byte{slaveID, FuncCodeReadHoldingRegisters}
+	data = append(data, uint162Bytes(address, quantity)...)
+	data = crc16(data)
+	c.Printf("ReadHoldingRegisters: 发送[% x]", data)
+	response, err := c.Send(data)
+	c.Printf("ReadHoldingRegisters: 接收[% x]", res)
+
+	// response, err := mb.send(&request)
+	if err != nil {
+		return
+	}
+	count := int(response.Data[0])
+	length := len(response.Data) - 1
+	if count != length {
+		err = fmt.Errorf("modbus: response data size '%v' does not match count '%v'", length, count)
+		return
+	}
+	results = response.Data[1:]
+	return
+}
+
+// Request:
 //
 //	Slave Id              : 1 byte
 //	Function code         : 1 byte (0x10)
